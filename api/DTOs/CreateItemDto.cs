da82f0f08912e6b5625c529b9b0ea125a2bc1baa
Bhuwan Vamsi <112535744+bhuwan-vamsi@users.noreply.github.com>
2025-07-29 17:34:48 +0530

    v1.1

--- api/DTOs/CreateItemDto.cs
+++
@@ -1,9 +0,0 @@
-namespace APIPractice.DTOs
-{
-    public class CreateItemDto
-    {
-        public string Itemname { get; set; } = string.Empty;
-        public int Price { get; set; }
-        public int Quantity { get; set; }
-    }
-}
